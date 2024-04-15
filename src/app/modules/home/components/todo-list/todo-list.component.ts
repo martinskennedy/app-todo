be008b71fe907577f059c38f1f12ab0901cd9c1b
Kennedy Martins <kemartins@gmail.com>
2024-04-15 09:14:26 -0300

    primeiro commit

---
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+
+//Interface
+import { TaskList } from '../../model/task-list';
+
+@Component({
+  selector: 'app-todo-list',
+  templateUrl: './todo-list.component.html',
+  styleUrls: ['./todo-list.component.scss']
+})
+export class TodoListComponent {
+
+  public taskList: Array<TaskList> = [
+    //{ task: "Minha nova Task", checked: true },
+    //{ task: "Minha nova Task 2", checked: false }
+  ];
+
+  public deleteItemTaskList(event: number){
+    this.taskList.splice(event, 1);
+  }
+
+  public deleteAllTaskList(){
+    const confirm = window.confirm("Você deseja realmente deletar tudo?");
+    if(confirm){
+      this.taskList = [];
+    }
+  }
+
+  public setEmitTaskList(event: string){
+    this.taskList.push({ task: event, checked: false });
+  }
+}
