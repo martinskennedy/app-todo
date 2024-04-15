341cad31889c0a2371b35f6a22c6ae60a44e4e1c
Kennedy Martins <kemartins@gmail.com>
2024-04-15 11:17:57 -0300

    ordenando itens checados, validando tarefa campo vazio

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -6,27 +6,42 @@ import { TaskList } from '../../model/task-list';
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
-  styleUrls: ['./todo-list.component.scss']
+  styleUrls: ['./todo-list.component.scss'],
 })
 export class TodoListComponent {
+  ngDoCheck() {
+    this.taskList.sort(
+      (first, last) => Number(first.checked) - Number(last.checked)
+    );
+  }
 
   public taskList: Array<TaskList> = [
     //{ task: "Minha nova Task", checked: true },
     //{ task: "Minha nova Task 2", checked: false }
   ];
 
-  public deleteItemTaskList(event: number){
+  public deleteItemTaskList(event: number) {
     this.taskList.splice(event, 1);
   }
 
-  public deleteAllTaskList(){
-    const confirm = window.confirm("Você deseja realmente deletar tudo?");
-    if(confirm){
+  public deleteAllTaskList() {
+    const confirm = window.confirm('Você deseja realmente deletar tudo?');
+
+    if (confirm) {
       this.taskList = [];
     }
   }
 
-  public setEmitTaskList(event: string){
+  public setEmitTaskList(event: string) {
     this.taskList.push({ task: event, checked: false });
+  }
+
+  public validationInput(event: string, index: number) {
+    if (!event.length) {
+      const confirm = window.confirm('Tarefa está vazia, deseja deletar?');
+      if (confirm) {
+        this.deleteItemTaskList(index);
+      }
+    }
   }
 }
